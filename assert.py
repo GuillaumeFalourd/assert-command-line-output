c601efb6a177ba4a6b5e8826ec394b8110564a4b
ArielMAJ <ariel.maj@hotmail.com>
2022-05-14 03:05:54 -0300

    Refactoring and small changes using black and pylint.

--- assert.py
+++ assert.py
@@ -35,7 +35,8 @@ import sys
 import os
 
 
-ENV_FILE = os.getenv('GITHUB_ENV')
+ENV_FILE = os.getenv("GITHUB_ENV")
+OUTPUT_FILE = "output.txt"
 
 
 def main():
@@ -46,13 +47,17 @@ def main():
     """
     if len(sys.argv) != 6:
         sys.exit(
-            f"Invalid usage: not enough arguments. \"Empty\" arguments should receive \"_\".\n\
-            \rExpected:\n{sys.argv[0]} \"<CommandLine>\" \"_<AssertFilePath>\" \"_<SpecificLineNumber>\" \"_<Contains>\" \"<ExpectedResult>\"\n"
+            f'Invalid usage: not enough arguments. "Empty" arguments should receive "_".\n\
+            \rExpected:\n\
+            {sys.argv[0]} "<CommandLine>" "_<AssertFilePath>" "_<SpecificLineNumber>" "_<Contains>" "<ExpectedResult>"\n'
         )
-    if not ((sys.argv[2][0] == '_') and (sys.argv[3][0] == '_') and (sys.argv[4][0] == '_')):
+    if not (
+        (sys.argv[2][0] == "_") and (sys.argv[3][0] == "_") and (sys.argv[4][0] == "_")
+    ):
         sys.exit(
-            f"Invalid usage: arguments in the middle should have a \"_\" before the actual input.\n\
-            \rExpected:\n{sys.argv[0]} \"<CommandLine>\" \"_<AssertFilePath>\" \"_<SpecificLineNumber>\" \"_<Contains>\" \"<ExpectedResult>\""
+            f'Invalid usage: arguments in the middle should have a "_" before the actual input.\n\
+            \rExpected:\n\
+            {sys.argv[0]} "<CommandLine>" "_<AssertFilePath>" "_<SpecificLineNumber>" "_<Contains>" "<ExpectedResult>"'
         )
 
     # The first and last argument will always exist given the workflow's
@@ -61,16 +66,16 @@ def main():
     # they do.
 
     input_command_line = sys.argv[1]
-    input_assert_file_path = sys.argv[2].replace('_', '', 1)
-    input_specific_line = sys.argv[3].replace('_', '', 1)
-    input_contains = sys.argv[4].replace('_', '', 1)
+    input_assert_file_path = sys.argv[2].replace("_", "", 1)
+    input_specific_line = sys.argv[3].replace("_", "", 1)
+    input_contains = sys.argv[4].replace("_", "", 1)
     input_expected_result = sys.argv[5]
 
     if input_specific_line:
         try:
             input_specific_line = int(input_specific_line)
         except ValueError:
-            sys.exit("Expected a number for \"specific line\".")
+            sys.exit('Expected a number for "specific line".')
 
     print("********************************")
     print(f"COMMAND LINE: {input_command_line}")
@@ -80,24 +85,23 @@ def main():
     print(f"EXPECTED RESULT: {input_expected_result}")
     print("********************************\n")
 
-    OUTPUT_FILE = "output.txt"
-    
     # Running cmd input and saving logs to OUTPUT_FILE.
     os.system(f"{input_command_line} > {OUTPUT_FILE}")
     # Reading OUTPUT_FILE and processing output text.
-    with open(OUTPUT_FILE, encoding='utf-8') as file:
+    with open(OUTPUT_FILE, encoding="utf-8") as file:
         actual_output = file.read()
     actual_output = process_text(actual_output)
-
 
     # Checking chosen option and getting the raw expected output.
     if input_contains:
         raw_expected_output = input_contains
     elif input_assert_file_path:
-        with open(input_assert_file_path, encoding='utf-8') as file:
+        with open(input_assert_file_path, encoding="utf-8") as file:
             raw_expected_output = file.read()
-        if type(input_specific_line) is int:
-            raw_expected_output = raw_expected_output.split('\n')[input_specific_line - 1]
+        if isinstance(input_specific_line, int):
+            raw_expected_output = raw_expected_output.split("\n")[
+                input_specific_line - 1
+            ]
     else:
         sys.exit("Not enough inputs.")
 
@@ -105,25 +109,23 @@ def main():
 
     # Checking if tests passed and comparing against expected result.
     test_result = "PASSED" if expected_output in actual_output else "FAILED"
-    print(
-        f"Expected: {input_expected_result}",
-        f"Actual: {test_result}",
-        sep='\n'
-    )
-    
+    print(f"Expected: {input_expected_result}", f"Actual: {test_result}", sep="\n")
+
     sys.exit(test_result != input_expected_result)
 
 
 def process_text(input_text):
-    """Removes trailing spaces, (extra) new lines ('\\n') and carriage return ('\\r') 
+    """Removes trailing spaces, (extra) new lines ('\\n') and carriage return ('\\r')
     \rfrom each line of some 'input_text' and returns it"""
 
-    processed_text = [line.strip(" \r\n") for line in input_text.strip(" \r\n").split("\n")]
+    processed_text = [
+        line.strip(" \r\n") for line in input_text.strip(" \r\n").split("\n")
+    ]
 
-    for _ in range(processed_text.count('')):
-        processed_text.remove('')
+    for _ in range(processed_text.count("")):
+        processed_text.remove("")
 
-    return '\n'.join(processed_text)
+    return "\n".join(processed_text)
 
 
 if __name__ == "__main__":
